a4d528aedabf639b3ad0b07e87bf0f2c40cdf71a
Agustin Ducca Pantaleon <social@agustinducca.com>
2019-02-20 01:13:15 -0300

    change csrf reference in create expedient form

--- expedient-manager/resources/views/expedients/create.blade.php
+++ expedient-manager/resources/views/expedients/create.blade.php
@@ -11,11 +11,14 @@
                         <div class="card-header">Ingreso de nuevo expediente</div>
                         <div class="card-body">
                             <form method="POST" action="{{ url('expedientes/creado') }}">
-                                {{ csrf_field() }}
+                                @csrf
                                 <div class="form-group row">
-                                    <label for="expedientNro" class="col-md-4 col-form-label text-md-right">Nro. Expediente</label>
+                                    <label for="expedientNro" class="col-md-4 col-form-label text-md-right">Nro.
+                                        Expediente</label>
                                     <div class="col-md-6">
-                                        <input type="number" name="expedientNro" value="{{old('expedientNro')}}" class="form-control {{ $errors->has('expedientNro') ? ' is-invalid' : '' }}" required autofocus>
+                                        <input type="number" name="expedientNro" value="{{old('expedientNro')}}"
+                                               class="form-control {{ $errors->has('expedientNro') ? ' is-invalid' : '' }}"
+                                               required autofocus>
                                         <span role="alert" class="invalid-feedback">
                                             @if ($errors->has('expedientNro'))
                                                 <strong>{{ $errors->first('expedientNro') }}</strong>
@@ -25,9 +28,12 @@
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label for="projectType" class="col-md-4 col-form-label text-md-right">Tipo de proyecto</label>
+                                    <label for="projectType" class="col-md-4 col-form-label text-md-right">Tipo de
+                                        proyecto</label>
                                     <div class="col-md-6">
-                                        <input type="text" name="projectType" value="{{ old('projectType') }}" class="form-control {{ $errors->has('projectType') ? ' is-invalid' : '' }}" required>
+                                        <input type="text" name="projectType" value="{{ old('projectType') }}"
+                                               class="form-control {{ $errors->has('projectType') ? ' is-invalid' : '' }}"
+                                               required>
                                         <span role="alert" class="invalid-feedback">
                                             @if ($errors->has('projectType'))
                                                 <strong>{{ $errors->first('projectType') }}</strong>
@@ -39,7 +45,9 @@
                                 <div class="form-group row">
                                     <label for="subject" class="col-md-4 col-form-label text-md-right">Asunto</label>
                                     <div class="col-md-6">
-                                        <input type="text" name="subject" value="{{ old('subject') }}" class="form-control {{ $errors->has('subject') ? ' is-invalid' : '' }}" required>
+                                        <input type="text" name="subject" value="{{ old('subject') }}"
+                                               class="form-control {{ $errors->has('subject') ? ' is-invalid' : '' }}"
+                                               required>
                                         <span role="alert" class="invalid-feedback">
                                             @if ($errors->has('subject'))
                                                 <strong>{{ $errors->first('subject') }}</strong>
@@ -52,7 +60,9 @@
                                 <div class="form-group row">
                                     <label for="cover" class="col-md-4 col-form-label text-md-right">Carátula</label>
                                     <div class="col-md-6">
-                                        <textarea rows="6" name="cover" id="cover" class="form-control {{ $errors->has('cover') ? ' is-invalid' : '' }}" style="resize: none;">{{ old('cover') }}</textarea>
+                                        <textarea rows="6" name="cover" id="cover"
+                                                  class="form-control {{ $errors->has('cover') ? ' is-invalid' : '' }}"
+                                                  style="resize: none;">{{ old('cover') }}</textarea>
                                         <span role="alert" class="invalid-feedback">
                                             @if ($errors->has('cover'))
                                                 <strong>{{ $errors->first('cover') }}</strong>
@@ -65,7 +75,9 @@
                                 <div class="form-group row">
                                     <label for="state" class="col-md-4 col-form-label text-md-right">Estado</label>
                                     <div class="col-md-6">
-                                        <select name="state" id="state" class="form-control {{ $errors->has('state') ? ' is-invalid' : '' }}" required>
+                                        <select name="state" id="state"
+                                                class="form-control {{ $errors->has('state') ? ' is-invalid' : '' }}"
+                                                required>
                                             @foreach(\App\Models\State::$states as $state)
                                                 <option value="{{$state}}">{{$state}}</option>
                                             @endforeach
@@ -80,9 +92,12 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label for="archived" class="col-md-4 col-form-label text-md-right">Archivado</label>
+                                    <label for="archived"
+                                           class="col-md-4 col-form-label text-md-right">Archivado</label>
                                     <div class="col-md-6">
-                                        <select name="archived" id="archived" class="form-control {{ $errors->has('archived') ? ' is-invalid' : '' }}" required>
+                                        <select name="archived" id="archived"
+                                                class="form-control {{ $errors->has('archived') ? ' is-invalid' : '' }}"
+                                                required>
                                             <option value=0>No</option>
                                             <option value=1>Si</option>
                                         </select>
@@ -96,9 +111,12 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label for="incomeRecord" class="col-md-4 col-form-label text-md-right">Registro de entrada</label>
+                                    <label for="incomeRecord" class="col-md-4 col-form-label text-md-right">Registro de
+                                        entrada</label>
                                     <div class="col-md-6">
-                                        <input type="text" name="incomeRecord" class="form-control {{ $errors->has('incomeRecord') ? ' is-invalid' : '' }}" value="{{ old('incomeRecord') }}">
+                                        <input type="text" name="incomeRecord"
+                                               class="form-control {{ $errors->has('incomeRecord') ? ' is-invalid' : '' }}"
+                                               value="{{ old('incomeRecord') }}">
                                         <span role="alert" class="invalid-feedback">
                                             @if ($errors->has('incomeRecord'))
                                                 <strong>{{ $errors->first('incomeRecord') }}</strong>
@@ -109,9 +127,12 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label for="treatmentRecord" class="col-md-4 col-form-label text-md-right">Registro de tratamiento</label>
+                                    <label for="treatmentRecord" class="col-md-4 col-form-label text-md-right">Registro
+                                        de tratamiento</label>
                                     <div class="col-md-6">
-                                        <input type="text" name="treatmentRecord" class="form-control {{ $errors->has('treatmentRecord') ? ' is-invalid' : '' }}" value="{{ old('treatmentRecord') }}">
+                                        <input type="text" name="treatmentRecord"
+                                               class="form-control {{ $errors->has('treatmentRecord') ? ' is-invalid' : '' }}"
+                                               value="{{ old('treatmentRecord') }}">
                                         <span role="alert" class="invalid-feedback">
                                             @if ($errors->has('treatmentRecord'))
                                                 <strong>{{ $errors->first('treatmentRecord') }}</strong>
